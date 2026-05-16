e1e1d086cba37509f1d02fdd29799d085c7456d7
Paulus sesa <114337187+seanpaulsesa@users.noreply.github.com>
2026-05-16 15:19:55 -0700

    added project laravel-12

---
+++ belajar-laravel/resources/views/pages/contact.blade.php
@@ -0,0 +1,14 @@
+<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="containe mt-3">
+    <h1>Anda Berada di Halaman Kontak</h1>
+</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
